84ae3b7e16f6826f922cc014912ae003afaf00a5
Claw76 <claw76@projectfoo.dev>
2023-07-11 04:45:28 +0200

    chore: commit to save

--- flake.nix
+++ flake.nix
@@ -3,7 +3,10 @@
 
   inputs = {
     # Specify the source of Home Manager and Nixpkgs.
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
+    nixpkgs = { 
+      url = "github:nixos/nixpkgs/nixos-23.05";
+      # config.allowUnfree = true;
+    };
     home-manager = {
       url = "github:nix-community/home-manager/release-23.05";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -24,6 +27,22 @@
 
         # Optionally use extraSpecialArgs
         # to pass through arguments to home.nix
+      };
+
+      packages.${system}.createHome = pkgs.writeShellApplication {
+        name = "create-home";
+        runtimeInputs = [ pkgs.cowsay ];
+        text = ''
+          # nix run github:Claw76/dotfiles#homeConfigurations.len.activationPackage
+          groupadd uinput
+          usermod -aG input "$USER"
+          usermod -aG uinput "$USER"
+          touch /etc/udev/rules.d/kanata.rules
+          echo "KERNEL=='uinput', MODE='0660', GROUP='uinput', OPTIONS+='static_node=uinput'" | \
+          sudo tee -a /etc/udev/rules.d/kanata.rules > /dev/null
+          modprobe uinput
+          systemctl --user enable kanata.service
+        '';
       };
     };
 }
